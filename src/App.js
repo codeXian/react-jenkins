9708ec77df943417cb1e9724d3824f60eaad7d5f
codexian <codexian@yeah.net>
2018-08-12 20:35:22 +0800

    app.js修改

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">我是react-jenkins78910</div>;
+    return <div className="App">codexian 出品必属精品</div>;
   }
 }
 
